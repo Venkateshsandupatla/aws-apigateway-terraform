aeeea3c4dba7c12f56e587126a4075792ee7ebc3
Venkateshsandupatla <66814938+Venkateshsandupatla@users.noreply.github.com>
2021-05-25 16:04:42 +0530

    Add files via upload

---
+++ apigateway.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region     = "ap-southeast-1"
+  access_key = "AKIATLFUJC7LMT6ZEECN"
+  secret_key = "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"
+}
+resource "aws_api_gateway_rest_api" "MyDemoAPI" {
+  name        = "MyDemoAPI"
+  description = "This is my API for demonstration purposes"
+}
+resource "aws_api_gateway_resource" "MyDemoResource" {
+  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
+  parent_id   = aws_api_gateway_rest_api.MyDemoAPI.root_resource_id
+  path_part   = "{proxy+}"
+}
+resource "aws_api_gateway_method" "MyDemoMethod" {
+  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
+  resource_id   = aws_api_gateway_resource.MyDemoResource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+   request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+resource "aws_api_gateway_integration" "MyDemoIntegration" {
+  rest_api_id          = aws_api_gateway_rest_api.MyDemoAPI.id
+  resource_id          = aws_api_gateway_resource.MyDemoResource.id
+  http_method          = aws_api_gateway_method.MyDemoMethod.http_method
+  type                 = "MOCK"
+ 
+}
+resource "aws_api_gateway_method_response" "response_404" {
+  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
+  resource_id = aws_api_gateway_resource.MyDemoResource.id
+  http_method = aws_api_gateway_method.MyDemoMethod.http_method
+  status_code = "404"
+}
+resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
+  resource_id = aws_api_gateway_resource.MyDemoResource.id
+  http_method = aws_api_gateway_method.MyDemoMethod.http_method
+  status_code = aws_api_gateway_method_response.response_404.status_code
+
+                                                                                                                                                                  
+                                                                                                                                                                                                                                                                                                                                                      
+}                                                                                                                                      
+                                                                                                                                                                           
+                                                                                                                                                                            
+                                                                                                                                                                         
